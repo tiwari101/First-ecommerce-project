fadd67a6dfebd1af660417f4bb3d1626f9ac42c0
Suraj tiwari <145787488+tiwari101@users.noreply.github.com>
2024-11-20 16:30:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+ document.getElementById("1").addEventListener('click',function(){
+    window.location.href='shop.html';
+});
+document.getElementById("2").addEventListener('click',function(){
+    window.location.href='shop.html';
+});
+document.getElementById("3").addEventListener('click',function(){
+    window.location.href='shop.html';
+});
+document.getElementById("4").addEventListener('click',function(){
+  window.location.href='shop.html';
+});
+
+document.getElementById("ram").addEventListener('click',function(){
+    window.location.href='shop.html';
+});
